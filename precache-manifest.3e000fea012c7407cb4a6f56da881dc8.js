70bdbfd9c93ead146a0bf0b6d937c9796f0cf26f
yelrik <4reg4444@gmail.com>
2019-05-19 12:48:32 +0300

    Updates

---
+++ precache-manifest.3e000fea012c7407cb4a6f56da881dc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkapptest/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "d9fee978e18ff85fb165",
+    "url": "/vkapptest/static/js/runtime~main.d9fee978.js"
+  },
+  {
+    "revision": "0bcba002f9ca341f02af",
+    "url": "/vkapptest/static/js/main.0bcba002.chunk.js"
+  },
+  {
+    "revision": "61999dee770e130dad72",
+    "url": "/vkapptest/static/js/2.61999dee.chunk.js"
+  },
+  {
+    "revision": "0bcba002f9ca341f02af",
+    "url": "/vkapptest/static/css/main.bea6ea30.chunk.css"
+  },
+  {
+    "revision": "61999dee770e130dad72",
+    "url": "/vkapptest/static/css/2.26939657.chunk.css"
+  },
+  {
+    "revision": "fbc63a5bb361bd91fbebb18147bbc558",
+    "url": "/vkapptest/index.html"
+  }
+];
